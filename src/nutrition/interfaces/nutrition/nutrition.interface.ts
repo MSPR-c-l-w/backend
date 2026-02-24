23b1c9141be05fa16fffde86171e8054204bde08
ThibaultCRRN <69095689+thibaultcrn1@users.noreply.github.com>
2026-02-24 11:35:18 +0100

    feat(etl): creation du pipeline etl pour nutrition (#17)
    * feat(etl): creation du pipeline etl pour nutrition
    
    * fix(request-changes): add requests changes

--- src/nutrition/interfaces/nutrition/nutrition.interface.ts
+++ src/nutrition/interfaces/nutrition/nutrition.interface.ts
@@ -3,9 +3,11 @@ import { Nutrition } from '@prisma/client';
 export interface INutritionController {
   getNutritions(): Promise<Nutrition[]>;
   getNutritionById(id: string): Promise<Nutrition>;
+  triggerImport(): Promise<{ message: string; count: number }>;
 }
 
 export interface INutritionService {
   getNutritions(): Promise<Nutrition[]>;
   getNutritionById(id: string): Promise<Nutrition>;
+  runImportPipeline(): Promise<number>;
 }
