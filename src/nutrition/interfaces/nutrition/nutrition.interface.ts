7b888c54760dde7dd3cfd8320027518523c2b1b6
Antoine Wacquiez <wacquiez.antoine@gmail.com>
2026-03-15 22:11:00 +0100

    Feature/nutrition update delete (#29)
    * Feature: updtade, delete nutriton
    
    * Feat: update, delete
    
    * Feat: update, delete

--- src/nutrition/interfaces/nutrition/nutrition.interface.ts
+++ src/nutrition/interfaces/nutrition/nutrition.interface.ts
@@ -1,13 +1,35 @@
 import { Nutrition } from '@prisma/client';
+import type { UpdateNutritionDto } from 'src/nutrition/dtos/update-nutrition.dto';
+
+export interface PaginatedNutritionResponse {
+  data: Nutrition[];
+  total: number;
+}
 
 export interface INutritionController {
-  getNutritions(): Promise<Nutrition[]>;
+  getNutritions(
+    page?: number,
+    limit?: number,
+  ): Promise<PaginatedNutritionResponse>;
   getNutritionById(id: string): Promise<Nutrition>;
+  updateNutrition(
+    id: string,
+    nutrition: UpdateNutritionDto,
+  ): Promise<Nutrition>;
+  deleteNutrition(id: string): Promise<Nutrition>;
   triggerImport(): Promise<{ message: string; count: number }>;
 }
 
 export interface INutritionService {
-  getNutritions(): Promise<Nutrition[]>;
+  getNutritions(
+    page?: number,
+    limit?: number,
+  ): Promise<PaginatedNutritionResponse>;
   getNutritionById(id: string): Promise<Nutrition>;
+  updateNutrition(
+    id: string,
+    nutrition: UpdateNutritionDto,
+  ): Promise<Nutrition>;
+  deleteNutrition(id: string): Promise<Nutrition>;
   runImportPipeline(): Promise<number>;
 }
