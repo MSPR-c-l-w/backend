bb009d4fe2a233c7243d3d2b5cdaf386677d8156
MisterX <palik.leote75@outlook.fr>
2026-05-28 22:33:39 +0200

    #130-Fix-error-lint

--- src/users/services/users/users.service.spec.ts
+++ src/users/services/users/users.service.spec.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { Prisma } from '@prisma/client';
